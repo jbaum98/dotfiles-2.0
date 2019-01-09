ec23cf717ae823b668f3386ed89225e251eeb40f
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-01-09 05:00:11 +0000

    Add nix-company

--- .config/nixpkgs/overlays/nix-mode.nix
+++ .config/nixpkgs/overlays/nix-mode.nix
@@ -2,6 +2,30 @@ self: super:
 {
   emacsPackagesNg = super.emacsPackagesNg // {
     melpaPackages = super.emacsPackagesNg.melpaPackages // {
+      nix-company = super.emacsPackagesNg.callPackage (
+      { emacs , fetchFromGitHub , fetchurl , lib , melpaBuild }:
+      melpaBuild {
+        pname = "nix-company";
+        ename = "nix-company";
+        version = "20190108.147";
+        src = fetchFromGitHub {
+          owner = "NixOS";
+          repo = "nix-mode";
+          rev = "54ef83310095689443c2371a312cc8687af6cbb9";
+          sha256 = "1n8nn3zq8skdb1q7vg5na7lfm2cf1hf8b2h8lmif27qz14lqsilr";
+        };
+        recipe = fetchurl {
+          url = "https://raw.githubusercontent.com/jbaum98/melpa/9cc2ff9e1731c975cc4f614f36b9466a8f3a0e06/recipes/nix-company";
+          sha256 = "1flkb57rzmx0pp9wpyld1hj1s5sndf9nymrd1mzpz3y6094l65sp";
+          name = "recipe";
+        };
+        packageRequires = [ emacs ];
+        meta = {
+          homepage = "https://melpa.org/#/nix-mode";
+          license = lib.licenses.free;
+        };
+      }) {};
+
       nix-mode = super.emacsPackagesNg.callPackage (
       { emacs , fetchFromGitHub , fetchurl , lib , melpaBuild }:
       melpaBuild {
