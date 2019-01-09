6e04a8ace4fe8af55f4778ec14cc71860e3bdd6a
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-01-09 03:45:04 +0000

    Add emacs.d/.gitignore

---
+++ .emacs.d/custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(c-basic-offset 2)
+ '(ispell-program-name "hunspell")
+ '(tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
