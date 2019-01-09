90595aeb641e6e45ab10db04c07838bb0ec8a608
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-01-09 05:04:34 +0000

    emacs: Add tabnine for nix

--- .emacs.d/lisp/lang/jw-lang-nix.el
+++ .emacs.d/lisp/lang/jw-lang-nix.el
@@ -7,34 +7,25 @@
 (eval-when-compile
   (require 'use-package))
 
+(eval-when-compile
+  (defvar company-backends))
+
+(defun jw--nix-mode-hook ()
+  "Hook to run in `nix-mode'."
+  (set (make-local-variable 'company-backends)
+       '(company-tabnine
+         company-capf
+         (company-abbrev company-dabbrev))))
+
 (use-package nix-mode
   :ensure
   :pin melpa
   :mode "\\.nix\\'"
+  :init
+  (add-hook 'nix-mode-hook #'jw--nix-mode-hook)
+  (add-hook 'nix-mode-hook #'company-mode)
   :custom
   (nix-indent-function #'nix-indent-line))
-
-;; (use-package nix-shell
-;;   :ensure nil
-;;   :commands (nix-shell nix-unpack))
-
-;; (use-package nix-buffer
-;;   :no-require
-;;   :ensure
-;;   :commands nix-buffer
-;;   :preface
-;;   (defun turn-on-nix-buffer ()
-;;     (when (and (not noninteractive)
-;;                (not (eq (aref (buffer-name) 0) ?\s))
-;;                (not (file-remote-p default-directory)))
-;;       (nix-buffer)))
-;;   :hook (after-change-major-mode . turn-on-nix-buffer))
-
-;; (use-package nix-update
-;;   :no-require
-;;   :ensure nil
-;;   :commands nix-update-fetch
-;;   :bind (("C-. u" . nix-update-fetch)))
 
 (provide 'jw-lang-nix)
 ;;; jw-lang-nix.el ends here
