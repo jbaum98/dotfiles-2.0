982f79f26dc800f6299df379fd23b2c2de51b214
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-01-09 03:46:07 +0000

    emacs: Add jw-lang-elisp

---
+++ .emacs.d/lisp/lang/jw-lang-elisp.el
@@ -0,0 +1,29 @@
+;;; jw-lang-elisp --- Emacs Lisp
+
+;;; Commentary:
+
+;;; Code:
+
+(eval-when-compile
+  (require 'use-package))
+
+(use-package company
+  :defer)
+
+(defun jw--emacs-lisp-mode-hook ()
+  "Hook to run on `emacs-lisp-mode'."
+  (setq company-transformers '(jw--company-sort-tabnine-first))
+  (set (make-local-variable 'company-backends)
+       '((company-capf    ; emacs lisp specific
+          :with
+          company-tabnine ; super smart
+         )
+         (company-abbrev company-dabbrev))))
+
+(use-package elisp-mode
+  :init
+  (add-hook 'emacs-lisp-mode-hook #'jw--emacs-lisp-mode-hook)
+  (add-hook 'emacs-lisp-mode-hook #'company-mode))
+
+(provide 'jw-lang-elisp)
+;;; jw-lang-elisp.el ends here
