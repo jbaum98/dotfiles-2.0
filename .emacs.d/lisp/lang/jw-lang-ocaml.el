73d635a1147cf300d040195b9c43645bd39fd9e5
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-02-10 04:20:25 +0000

    stash

--- .emacs.d/lisp/lang/jw-lang-ocaml.el
+++ .emacs.d/lisp/lang/jw-lang-ocaml.el
@@ -14,17 +14,20 @@
   (defun jw--tuareg-mode-hook ()
     "Hook to run on entering tuareg mode."
     (when (functionp 'prettify-symbols-mode)
-      (prettify-symbols-mode)))
+      (prettify-symbols-mode))
+    (lsp)
+    (lsp-ui-flycheck-enable 1)
+    )
   :hook (tuareg-mode . jw--tuareg-mode-hook))
 
 (use-package projectile :ensure)
 (use-package spinner :ensure :pin elpa)
 (use-package lsp-mode
   :ensure
-  :after spinner
-  :hook
-  (tuareg-mode . lsp)
-  (caml-mode . lsp))
+  :commands lsp
+  :after spinner)
+(use-package lsp-ui-flycheck
+  :commands lsp-ui-flycheck-enable)
 
 (use-package lsp-ui
   :ensure
