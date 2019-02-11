058b43caca5d3cc9ee7222e8214f072ead84e061
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-02-11 08:25:28 +0000

    Add elisp mode back

--- .emacs.d/lisp/lang/jw-lang-elisp.el
+++ .emacs.d/lisp/lang/jw-lang-elisp.el
@@ -7,9 +7,6 @@
 (eval-when-compile
   (require 'use-package))
 
-(use-package company
-  :defer)
-
 (defun jw--emacs-lisp-mode-hook ()
   "Hook to run on `emacs-lisp-mode'."
   (set (make-local-variable 'company-transformers)
@@ -22,9 +19,10 @@
          (company-abbrev company-dabbrev))))
 
 (use-package elisp-mode
-  :init
-  (add-hook 'emacs-lisp-mode-hook #'jw--emacs-lisp-mode-hook)
-  (add-hook 'emacs-lisp-mode-hook #'company-mode))
+  :hook
+  (emacs-lisp-mode . jw--emacs-lisp-mode-hook)
+  (emacs-lisp-mode . company-mode)
+  (emacs-lisp-mode . flycheck-mode))
 
 (provide 'jw-lang-elisp)
 ;;; jw-lang-elisp.el ends here
