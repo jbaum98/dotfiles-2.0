422ca45035a7325e2a59bbbdc812c59b0afd773c
Jake Waksbaum <jake.waksbaum@gmail.com>
2019-02-20 19:05:54 +0000

    Fix ocaml flycheck

--- .emacs.d/lisp/lang/jw-lang-ocaml.el
+++ .emacs.d/lisp/lang/jw-lang-ocaml.el
@@ -20,10 +20,10 @@
   spinner-stop)
 
 (use-package lsp-mode
-  :commands lsp)
+  :hook (tuareg-mode . lsp))
 
 (use-package lsp-ui
-  :commands lsp-ui-mode
+  :hook (tuareg-mode . lsp-ui-mode)
   :custom-face
   (lsp-ui-sideline-current-symbol
    ((t (:height 0.99
@@ -31,14 +31,20 @@
         :box (:line-width -1 :color "dim gray" :style nil)
         :foreground "dim gray")))))
 
+(use-package lsp-ui-flycheck
+  :commands lsp-ui-flycheck-enable
+  :hook (tuareg-mode . (lambda () (lsp-ui-flycheck-enable 1))))
+
 (use-package company-lsp :commands company-lsp)
 
-(use-package tuareg
-  :mode ("\\.ml\\'" . tuareg-mode)
-  :hook (tuareg-mode . prettify-symbols-mode)
-  :hook (tuareg-mode . lsp)
-  :hook (tuareg-mode . lsp-ui-mode))
+(use-package flycheck
+  :hook (tuareg-mode . flycheck-mode))
 
+(use-package prog-mode
+  :hook (tuareg-mode . prettify-symbols-mode))
+
+(use-package tuareg
+  :mode ("\\.ml\\'" . tuareg-mode))
 
 (provide 'jw-lang-ocaml)
 ;;; jw-lang-ocaml ends here
